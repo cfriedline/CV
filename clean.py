92caff08109ce2994288d4658eee9d2b31d737f9
Chris Friedline <cfriedline@vcu.edu>
2015-02-04 22:17:13 -0500

    add clean.py to remove address field for website dist

---
+++ clean.py
@@ -0,0 +1,10 @@
+def main():
+	with open("friedline_cv_clean.tex", "w") as o:
+		for line in open("friedline_cv.tex"):
+			line = line.strip()
+			if '\\address' in line:
+				line = "\\address{hidden}{hidden}"
+			o.write("%s\n" % line)
+
+if __name__ == '__main__':
+	main()
